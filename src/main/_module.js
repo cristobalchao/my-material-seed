60045f4706c690ffc0598ab67aa9b6005f0283a2
cristobalchao@google.com <cristobalchao@google.com>
2017-02-24 21:29:54 -0800

    Added eslint-angular configuration

--- src/main/_module.js
+++ src/main/_module.js
@@ -5,4 +5,7 @@
 import MainController from './controllers/MainController';
 
 export default angular.module('myMaterialSeed.main', [] )
-    .controller('MainController', MainController);
+    .controller('MainController', MainController)
+    .controller('directiveTwo', function() {
+      console.log('hello');
+    });
